5bd26858346b5181e087ecd3d5c7b872dd06ed54
ad01299 <ad01299@surrey.ac.uk>
2020-12-12 23:35:44 +0000

    Fixed logging in issue, app will now show logout button, also added dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :properties
+  get "/dashboard" => 'dashboard#index', as: :dashboard
+  get 'dashboard/properties'
+  get 'dashboard/reports'
+  
+
   devise_for :accounts
+  resources :properties
   root to: 'public#main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
