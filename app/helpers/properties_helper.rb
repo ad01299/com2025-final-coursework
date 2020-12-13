da4254fcc0236f680d9de94247b446b203613d9e
ad01299 <ad01299@surrey.ac.uk>
2020-12-13 16:57:32 +0000

    added image slideshow and fixed home page layout

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,7 @@
 module PropertiesHelper
+
+	def property_thumbnail property_thumbnail
+	  img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
+	  image_tag img, class: "property-thumb"
+	end
 end
