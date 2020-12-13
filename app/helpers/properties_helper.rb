850ae3e65ef227d45d8860da9a11aba550add42e
ad01299 <ad01299@surrey.ac.uk>
2020-12-13 20:25:38 +0000

    added photos for properties

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,7 +1,7 @@
 module PropertiesHelper
 
-	def property_thumbnail property_thumbnail
-	  img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
-	  image_tag img, class: "property-thumb"
-	end
+  def property_thumbnail property
+  	img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
+  	image_tag img, class: "property-thumb"
+  end
 end
