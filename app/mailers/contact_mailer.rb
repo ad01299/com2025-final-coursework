8ea1040d62fbee88c03b39854550d5b87b23f068
ad01299 <ad01299@surrey.ac.uk>
2020-12-14 14:08:33 +0000

    final commit, got rid of unused links/pages, finished styling the contact page and made sure it could send an email out to cosyhomes email, styled the forgot password page to fit rest of site

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: # my email address
+    default to: cosyhomes@hotmail.com
 
     def contact_email(name, email, body)
         @name = name
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,9 @@
 class Contact < MailForm::Base
   attribute :name, :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :subject, :validate => true
   attribute :message,   :validate => true
-  attribute :nickname,  :captcha  => true
+  
 
   def headers
     {
